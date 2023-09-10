3398063c64cbbb7259a46b9f9b0129f4d4bf5c79
lliikko <angelikayangirova@gmail.com>
2023-09-10 14:43:07 +0500

    4

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
+const cors = require('cors');
 const error = require('./middlewares/error');
 const routes = require('./routes');
 
@@ -21,6 +22,24 @@ mongoose.connect(host, {
 });
 app.use(cookieParser());
 app.use(bodyParser.json());
+
+const allowedCors = [
+  'https://angelikayang.nomoreparties.co/', 'http://angelikayang.nomoreparties.co/',
+  'http://localhost:3001',
+];
+
+app.use(cors());
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  next();
+});
+
 app.use(requestLogger);
 app.use(limiter);
 
--- utils/config.js
+++ utils/config.js
@@ -1,2 +1,2 @@
-module.exports.port = '5000';
+module.exports.port = '3001';
 module.exports.host = 'mongodb://127.0.0.1:27017/bitfilmsdb';

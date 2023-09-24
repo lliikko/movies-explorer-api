dbeef304beb22b550e38504c06beeddc621dcf29
lliikko <angelikayangirova@gmail.com>
2023-09-24 14:43:30 +0500

    fvudbciebfefbcileybcfdyh

--- controllers/users.js
+++ controllers/users.js
@@ -71,7 +71,7 @@ module.exports.login = (req, res, next) => {
       return res
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
-          SameSite: 'none',
+          SameSite: true,
         })
         .send({
           token,
